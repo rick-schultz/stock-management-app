37eadaaa21c4d1979256d22771ac392d60a9604c
Rick Schultz <rick-schultz@hotmail.com>
2022-08-07 23:24:36 +0200

    ✨ feat: create vuex store
    created 2 global states, 3 actions and 2 mutations.
    I sliced data from api because render was slow.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,51 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+
+const url = 'http://localhost:8080/api/product/'
 
 export default createStore({
   state: {
+    products: [],
+    product: []
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setProducts(state, payload) {
+      state.products = payload
+    },
+    setProduct(state, payload) {
+      state.product = payload
+    }
   },
   actions: {
+    async getProducts({ commit }) {
+      try {
+        const productsList = await axios.get(`${url}`)
+        const slicedList = productsList.data.slice(0, 10)
+        console.log(slicedList)
+        commit('setProducts', slicedList)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async getProduct({ commit }, value) {
+      try {
+        const productList = await axios.get(`${url}${value}`)
+        console.log(productList.data)
+        commit('setProduct', productList.data)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async updateStock({ commit }, value) {
+      try {
+        const updatedProduct = await axios.put(`${url}${value}`)
+        console.log(updatedProduct.data)
+        commit('setProduct', updatedProduct.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
