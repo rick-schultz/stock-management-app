3d9d72de611c09eb176a77811d8d81dfed38fdf3
Rick Schultz <rick-schultz@hotmail.com>
2022-08-08 00:06:12 +0200

    ✨ feat: create stock page
    create stock view where the user can see the stock quantity
    create button to go the edit product page

--- src/views/StockView.vue
+++ src/views/StockView.vue
@@ -1,5 +1,62 @@
 <template>
-  <div class="about">
-    <h1>This is the Stock page</h1>
-  </div>
+  <v-main class="pt-1 main">
+    <v-container fluid>
+      <v-row dense>
+        <v-col
+          v-for="product in $store.state.products"
+          :key="product.id"
+          cols="12"
+          xs="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
+          <v-card
+            :title="product.name"
+            :text="`Current stock: ${product.stock}`"
+          >
+            <v-btn color="success" class="mx-4 mb-4">
+              <router-link
+                to="/product"
+                class="link"
+                @click="this.getProduct(product.id)"
+                >Edit Stock</router-link
+              >
+            </v-btn>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import { mapActions } from 'vuex'
+
+export default defineComponent({
+  name: 'StockView',
+
+  mounted() {
+    this.getProducts('')
+  },
+  methods: {
+    ...mapActions(['getProducts']),
+    ...mapActions(['getProduct']),
+    validate() {
+      this.$refs.form.validate()
+    }
+  },
+  watch: {
+    group() {
+      this.drawer = false
+    }
+  }
+})
+</script>
+<style scoped>
+.link {
+  text-decoration: none;
+  color: #000;
+}
+</style>
