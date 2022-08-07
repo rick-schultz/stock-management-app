dc0fbdcf01d0c8c29abba554d11fb72bf5915d27
Rick Schultz <rick-schultz@hotmail.com>
2022-08-08 00:08:44 +0200

    ✨ feat: edit product page
    get info about product from global state
    add quantity to stock
    clean input and render changes

--- src/views/EditProductView.vue
+++ src/views/EditProductView.vue
@@ -1,5 +1,72 @@
 <template>
-  <div class="about">
-    <h1>This is the Edit Product page</h1>
-  </div>
+  <v-main class="pt-1 main">
+    <v-container fluid>
+      <v-row dense>
+        <v-col cols="12" xs="12" sm="12" md="6" lg="6">
+          <v-card :title="$store.state.product.name">
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-row class="pa-2 align-center">
+                <v-col cols="7">
+                  <v-text-field
+                    rounded
+                    hide-details
+                    v-model="stock"
+                    :rules="stockRules"
+                    :label="`Current stock: ${$store.state.product.stock}`"
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="5">
+                  <v-btn
+                    :disabled="!valid"
+                    color="success"
+                    @click="
+                      ;[
+                        this.updateStock(
+                          `${$store.state.product.id}/refill?amount=${this.stock}`
+                        ),
+                        this.clearInput()
+                      ]
+                    "
+                  >
+                    Add to Stock
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import { mapActions } from 'vuex'
+
+export default defineComponent({
+  name: 'EditProductView',
+  data: () => ({
+    valid: true,
+    stock: '',
+    stockRules: [(v) => !!v || 'Stock is required']
+  }),
+
+  methods: {
+    ...mapActions(['updateStock']),
+    validate() {
+      this.$refs.form.validate()
+    },
+    clearInput() {
+      this.stock = ''
+      this.stockRules = [true]
+    }
+  },
+  watch: {
+    group() {
+      this.drawer = false
+    }
+  }
+})
+</script>
